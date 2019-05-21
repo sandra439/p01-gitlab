f8151a96a20a0ebfa891cdb4d85c6b8ac025e125
sandra439 <50861661+sandra439@users.noreply.github.com>
2019-05-21 09:23:04 +0200

    Update 01program.php

--- 01program.php
+++ 01program.php
@@ -4,4 +4,5 @@ echo("<br> Jestem programem 01");
 echo ("<br>Nie wiem co sie tu robi");
 echo ("<br> To jest dziwne");
 echo ("<br> Dalsza część programu");
+echo ("<br> W końcu coś działa :)");
 ?>
